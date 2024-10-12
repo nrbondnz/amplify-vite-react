973ddcdd96a07ca62106ff8f9e0c8dafb6a6fcce
Bond in Auckland <nigel.bond@orcon.net.nz>
2024-10-13 10:37:13 +1300

    ready to mark as release

---
+++ src/aws-exports.ts
@@ -0,0 +1,10 @@
+﻿const awsExports = {
+	aws_project_region: 'ap-southeast-2',
+	aws_cognito_identity_pool_id: 'ap-southeast-2:1402af86-7d2a-43df-901e-0ed7af74bb95',
+	aws_cognito_region: 'ap-southeast-2',
+	aws_user_pools_id: 'ap-southeast-2_GMoWoGim5',
+	aws_user_pools_web_client_id: '3rut7c7dmofprou7j0197g1tq5',
+	oauth: {},
+};
+
+export default awsExports;
