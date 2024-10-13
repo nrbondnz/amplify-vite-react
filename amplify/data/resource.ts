bbb6c22652ac7320bc720801df8cf1396b3905a6
Bond in Auckland <nigel.bond@orcon.net.nz>
2024-10-13 17:03:46 +1300

    Refactor schema setup by moving to a separate file
    Moved the schema definition from resource.ts to a new schema.ts file to improve code modularity and maintainability. Adjusted the import paths accordingly to ensure seamless integration.

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,4 +1,5 @@
-import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
+import { type ClientSchema, defineData } from "@aws-amplify/backend";
+import schema from "./schema"; // Adjust the import path as necessary
 
 /*== STEP 1 ===============================================================
 The section below creates a Todo database table with a "content" field. Try
@@ -6,13 +7,6 @@ adding a new "isDone" field as a boolean. The authorization rule below
 specifies that any user authenticated via an API key can "create", "read",
 "update", and "delete" any "Todo" records.
 =========================================================================*/
-const schema = a.schema({
-  Todo: a
-    .model({
-      content: a.string(),
-    })
-    .authorization((allow) => [allow.publicApiKey()]),
-});
 
 export type Schema = ClientSchema<typeof schema>;
 
---
+++ amplify/data/schema.ts
@@ -0,0 +1,11 @@
+﻿import { a } from "@aws-amplify/backend";
+
+const schema = a.schema({
+	Todo: a
+		.model({
+			content: a.string(),
+		})
+		.authorization((allow) => [allow.publicApiKey()]),
+});
+
+export default schema;
